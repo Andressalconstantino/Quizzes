d62ce2c0cbb345c8b41806b5cd3b9a414d63f28d
Andressalconstantino <86380516+Andressalconstantino@users.noreply.github.com>
2021-11-28 21:50:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+document.getElementById('one').addEventListener('click', function (){
+    document.getElementById('First').setAttribute('class', 'check');
+    document.getElementById('Second').setAttribute('class', 'notclicked');
+    document.getElementById('Third').setAttribute('class', 'notclicked');
+    document.getElementById('Fourth').setAttribute('class', 'notclicked');
+});
+
+document.getElementById('two').addEventListener('click', function (){
+    document.getElementById('First').setAttribute('class', 'notclicked');
+    document.getElementById('Second').setAttribute('class', 'check');
+    document.getElementById('Third').setAttribute('class', 'notclicked');
+    document.getElementById('Fourth').setAttribute('class', 'notclicked');
+});
+
+document.getElementById('three').addEventListener('click', function (){
+    document.getElementById('First').setAttribute('class', 'notclicked');
+    document.getElementById('Second').setAttribute('class', 'notclicked');
+    document.getElementById('Third').setAttribute('class', 'check');
+    document.getElementById('Fourth').setAttribute('class', 'notclicked');
+});
+
+document.getElementById('four').addEventListener('click', function (){
+    document.getElementById('First').setAttribute('class', 'notclicked');
+    document.getElementById('Second').setAttribute('class', 'notclicked');
+    document.getElementById('Third').setAttribute('class', 'notclicked');
+    document.getElementById('Fourth').setAttribute('class', 'check');
+});
+
+/* -------------------- SECOND QUESTION C++ -------------------- */
+
+document.getElementById('oneD').addEventListener('click', function (){
+    document.getElementById('First').setAttribute('class', 'check');
+    document.getElementById('Second').setAttribute('class', 'notclicked');
+    document.getElementById('Third').setAttribute('class', 'notclicked');
+    document.getElementById('Fourth').setAttribute('class', 'notclicked');
+});
+
+document.getElementById('twoD').addEventListener('click', function (){
+    document.getElementById('First').setAttribute('class', 'notclicked');
+    document.getElementById('Second').setAttribute('class', 'check');
+    document.getElementById('Third').setAttribute('class', 'notclicked');
+    document.getElementById('Fourth').setAttribute('class', 'notclicked');
+});
+
+document.getElementById('threeD').addEventListener('click', function (){
+    document.getElementById('First').setAttribute('class', 'notclicked');
+    document.getElementById('Second').setAttribute('class', 'notclicked');
+    document.getElementById('Third').setAttribute('class', 'check');
+    document.getElementById('Fourth').setAttribute('class', 'notclicked');
+});
+
+document.getElementById('fourD').addEventListener('click', function (){
+    document.getElementById('First').setAttribute('class', 'notclicked');
+    document.getElementById('Second').setAttribute('class', 'notclicked');
+    document.getElementById('Third').setAttribute('class', 'notclicked');
+    document.getElementById('Fourth').setAttribute('class', 'check');
+});
